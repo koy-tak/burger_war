e691c397eaba827e0b78c2ecdc8e0e320ad47bdf
koy-tak <45578558+koy-tak@users.noreply.github.com>
2019-08-17 09:56:06 +0900

    revised move strategy.

--- burger_war/scripts/randomCcr.py
+++ burger_war/scripts/randomCcr.py
@@ -23,12 +23,16 @@ class RandomBot(AbstractCcr):
     '''
 
     def find_red_ball(self):
-        hsv_img = cv2.cvtColor(self.img_orig, cv2.COLOR_BGR2HSV)
+	self.img = cv2.rectangle(self.img, (0,240), (640,480), (0,0,0), -1)
+#        color_min = np.array([0,0,100])
+#        color_max = np.array([100,100,255])
+#       color_mask = cv2.inRange(self.img, color_min, color_max)
+        hsv_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
         color_min = np.array([0,100,150])
         color_max = np.array([50,255,255])
         color_mask = cv2.inRange(hsv_img, color_min, color_max)
 #        cv2.imwrite("/home/koy/catkin_ws/src/burger_war/color_mask.bmp", color_mask)
-        bin_img = cv2.bitwise_and(self.img_orig, self.img_orig, mask = color_mask)
+        bin_img = cv2.bitwise_and(self.img, self.img, mask = color_mask)
 #        cv2.imwrite("/home/koy/catkin_ws/src/burger_war/bin.bmp", bin_img)
         bin_img = cv2.cvtColor(bin_img, cv2.COLOR_BGR2GRAY)
         nLabels, label_img, data, center = cv2.connectedComponentsWithStats(bin_img)
@@ -37,7 +41,7 @@ class RandomBot(AbstractCcr):
 #        for i in range(nLabels - 1):
 #            print(data[i])
         if nLabels < 2:
-            print("Labeling: NO: ", nLabels)
+#            print("Labeling: NO: ", nLabels)
             return (-2.0, -2.0)
        
         size_max = 0
@@ -45,16 +49,15 @@ class RandomBot(AbstractCcr):
         size_max_y = 0
         size_max_w = 0
         size_max_h = 0
-        for i in range(1, nLabels - 1):
+        for i in range(1, nLabels):
             x, y, w, h, size = data[i]
-            print("Labeling: Loop", i, nLabels, size_max, size)
             if size > size_max:
                 size_max_x = x
-                size_max_y = y
-                size_max_w = w
-                size_max_h = h
-                size_max = size
-#                print("Labeling: ", i, nLabels, size_max_x, size_max_y, size_max_w, size_max_h, size_max)
+		size_max_y = y
+		size_max_w = w
+		size_max_h = h
+		size_max = size
+#                print("Labeling: ", i, size_max_x, size_max_y, size_max_w, size_max_h, size_max)
 
         hrz = float(size_max_x + size_max_w/2 - 320) / 320.0
         vrt = float(size_max_y + size_max_h/2 - 240) / 240.0
@@ -62,53 +65,81 @@ class RandomBot(AbstractCcr):
 
         return (hrz, vrt)
 
-    
+
     def strategy(self):
         r = rospy.Rate(100)
 
         UPDATE_FREQUENCY = 1 # 1sec
         update_time = 0
 
-        img_cntr_prev = 0
-
-#        self.img_orig = cv2.imread("/home/koy/catkin_ws/src/burger_war/sample.png")
-#        print(self.img_orig)
-#        self.img_cntr = 1
+	img_cntr_prev = 0
+	lidar_cntr_prev = 0
 
         while not rospy.is_shutdown():
-            if self.left_bumper or self.right_bumper:
+#            if self.left_bumper or self.right_bumper:
+#                update_time = time.time()
+#                rospy.loginfo('bumper hit!!')
+#                x = -0.2
+#                th = 0
+
+	    if self.lidar_cntr > 0 and self.scan.ranges[0] < 0.2:
                 update_time = time.time()
                 rospy.loginfo('bumper hit!!')
                 x = -0.2
-                th = 0
+		if self.lidar_cntr % 2 == 0:
+                    th = 2
+		else:
+                    th = -2
 
-            elif self.img_cntr != img_cntr_prev:
-                img_cntr_prev = self.img_cntr
-                hrz, vrt = self.find_red_ball()
-                x = 0
-                th = 0
+	    else:
+		hrz = -2.0
+		vrt = -2.0
 
-            elif time.time() - update_time > UPDATE_FREQUENCY:
-                update_time = time.time()
+		if self.img_cntr != img_cntr_prev:
+		    img_cntr_prev = self.img_cntr
+		    hrz, vrt = self.find_red_ball()
+		    print(hrz, vrt)
+
                 value = random.randint(1,1000)
-                # go
-                if value < 500:
-                    x = 0.2
+                print(value)
+
+                if hrz == 0:
+                    x = 0.4
                     th = 0
-
-                # turn left
-                elif value < 750:
-                    x = 0
-                    th = 1
-
-                # turn right
-                elif value < 1000:
-                    x = 0
-                    th = -1
-
+                    update_time = time.time()
+                    print("A")
+                elif hrz == -2:
+		    if time.time() - update_time > UPDATE_FREQUENCY:
+                        update_time = time.time()
+                        if value < 500:
+                            x = 0.4
+                            th = 0
+                            print("B")
+                        elif value < 750:
+                            x = 0
+                            th = 2
+                            print("C")
+                        elif value < 1000:
+                            x = 0
+                            th = -2
+                            print("D")
+                        else:
+                           x = 0
+                           th = 0
+                           print("E")
+                elif hrz < 0:
+                    x = 0.4
+                    th = 0.1
+                    print("F")
+                elif hrz > 0:
+                    x = 0.4
+                    th = -0.1
+                    print("G")
                 else:
                     x = 0
                     th = 0
+                    print("H")
+
 
             twist = Twist()
             twist.linear.x = x; twist.linear.y = 0; twist.linear.z = 0
@@ -119,5 +150,5 @@ class RandomBot(AbstractCcr):
 
 if __name__ == '__main__':
     rospy.init_node('random_ccr')
-    bot = RandomBot(use_bumper=True, use_camera=True, camera_preview=True)
+    bot = RandomBot(use_bumper=True, use_camera=True, camera_preview=True, use_lidar=True)
     bot.strategy()
